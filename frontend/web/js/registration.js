adb1eb0c2053bc8693df83eb340c456b914c29b7
Kamal Sharma <kamaldgrt@gmail.com>
2020-07-03 23:49:30 +0530

    Update registration.js

--- frontend/web/js/registration.js
+++ frontend/web/js/registration.js
@@ -22,7 +22,7 @@ function allowAlphabets(e) {
         return false;
 }
 
--//jQuery time
+//jQuery time
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
