92f840f7034d096e83a1294e12a3efe0d2948997
05saitejaswi <saitejaswi.chakravaram05@gmail.com>
2020-07-12 19:13:09 +0530

    added faqs of webworld

--- frontend/views/event/webworld.php
+++ frontend/views/event/webworld.php
@@ -11,19 +11,19 @@ $this->title = 'Web World';
 EventAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-    <!DOCTYPE html>
-    <html lang="<?= Yii::$app->language ?>">
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
 
-    <head>
-        <meta charset="<?= Yii::$app->charset ?>">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php $this->registerCsrfMetaTags() ?>
-        <title><?= Html::encode($this->title) ?></title>
-        <?php $this->head() ?>
-    </head>
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
 
-    <body>
+<body>
     <?php $this->beginBody() ?>
     <!-- Start Header -->
     <header id="mu-hero" class="" role="banner">
@@ -76,7 +76,7 @@ EventAsset::register($this);
                             <div class="row">
                                 <div class="col-md-6 col-sm-12">
                                     <div class="mu-about-left">
-                                        <img class="animated slideInLeft" src="<?php echo Yii::$app->params['frontendUrl'] . 'event/images/event_robokart.png';?>" alt="Men Speaker">
+                                        <img class="animated slideInLeft" src="<?php echo Yii::$app->params['frontendUrl'] . 'event/images/event_robokart.png'; ?>" alt="Men Speaker">
                                     </div>
                                 </div>
                                 <div class="col-md-6 col-sm-12">
@@ -85,18 +85,19 @@ EventAsset::register($this);
                                             <h2 style="text-decoration: underline;">Description :-</h2>
 
                                             <p>
-                                                Since its inception, websites have been the foundation of companies and organizations galore. 
+                                                Since its inception, websites have been the foundation of companies and organizations galore.
                                                 Today it's your turn to bring about that difference!
                                             </p><br>
                                             <p>
-                                                In mere 6 hours, you'll be given a chance to showcase your ideas and talents through your website. 
-                                                Bring your finest code to the room and create your own website from scratch to win the glory of the 
+                                                In mere 6 hours, you'll be given a chance to showcase your ideas and talents through your website.
+                                                Bring your finest code to the room and create your own website from scratch to win the glory of the
                                                 Web World!
                                             </p>
                                         </div>
                                     </div>
 
-                                    <div class="row"> <!-- Row starts -->
+                                    <div class="row">
+                                        <!-- Row starts -->
                                         <div class="col-md-12">
                                             <div class="mu-sponsors-area animated zoomInRight">
                                                 <p class="myAccordion">Rules and Regulations</p>
@@ -114,7 +115,7 @@ EventAsset::register($this);
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>     <!-- Row ends -->
+                                    </div> <!-- Row ends -->
 
                                     <div class="row">
                                         <div class="col-md-12">
@@ -126,16 +127,17 @@ EventAsset::register($this);
                                                         <div class="panel panel-default animated zoomInLeft">
                                                             <div class="panel-heading">
                                                                 <h4 class="panel-title">
-                                                                    <a data-toggle="collapse" data-parent="#accordion"
-                                                                        href="#collapseOne" aria-expanded="true">
+                                                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true">
                                                                         <span class="fa fa-angle-up"></span>
                                                                         Question 1
+                                                                        How should I design the website?
                                                                     </a>
                                                                 </h4>
                                                             </div>
                                                             <div id="collapseOne" class="panel-collapse collapse in">
                                                                 <div class="panel-body">
                                                                     Answer 1
+                                                                    Every participant will receive a template to their registered mail id.
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -143,16 +145,17 @@ EventAsset::register($this);
                                                         <div class="panel panel-default animated slideInLeft">
                                                             <div class="panel-heading">
                                                                 <h4 class="panel-title">
-                                                                    <a data-toggle="collapse" data-parent="#accordion"
-                                                                        href="#collapseTwo">
+                                                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
                                                                         <span class="fa fa-angle-up"></span>
                                                                         Question 2
+                                                                        How can I submit my final code?
                                                                     </a>
                                                                 </h4>
                                                             </div>
                                                             <div id="collapseTwo" class="panel-collapse collapse">
                                                                 <div class="panel-body">
                                                                     Answer 2
+                                                                    Completed code should be submitted in the Google Classroom, which will be provided after registration.
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -160,16 +163,17 @@ EventAsset::register($this);
                                                         <div class="panel panel-default animated slideInRight">
                                                             <div class="panel-heading">
                                                                 <h4 class="panel-title">
-                                                                    <a data-toggle="collapse" data-parent="#accordion"
-                                                                        href="#collapseThree">
+                                                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
                                                                         <span class="fa fa-angle-up"></span>
                                                                         Question 3
+                                                                        How will I receive my certificate?
                                                                     </a>
                                                                 </h4>
                                                             </div>
                                                             <div id="collapseThree" class="panel-collapse collapse">
                                                                 <div class="panel-body">
                                                                     Answer 3
+                                                                    E-certificates will be sent after the successful completion of the event via mail. All winners and participants will be awarded with certificates.
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -177,16 +181,17 @@ EventAsset::register($this);
                                                         <div class="panel panel-default animated slideInLeft">
                                                             <div class="panel-heading">
                                                                 <h4 class="panel-title">
-                                                                    <a data-toggle="collapse" data-parent="#accordion"
-                                                                        href="#collapseFour">
+                                                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
                                                                         <span class="fa fa-angle-up"></span>
                                                                         Question 4
+                                                                        What will be the total duration of the competition?
                                                                     </a>
                                                                 </h4>
                                                             </div>
                                                             <div id="collapseFour" class="panel-collapse collapse">
                                                                 <div class="panel-body">
                                                                     Answer 4
+                                                                    6 hours.
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -194,16 +199,17 @@ EventAsset::register($this);
                                                         <div class="panel panel-default animated slideInRight">
                                                             <div class="panel-heading">
                                                                 <h4 class="panel-title">
-                                                                    <a data-toggle="collapse" data-parent="#accordion"
-                                                                        href="#collapseFive">
+                                                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive">
                                                                         <span class="fa fa-angle-up"></span>
                                                                         Question 5
+                                                                        What if my Internet connection is unstable?
                                                                     </a>
                                                                 </h4>
                                                             </div>
                                                             <div id="collapseFive" class="panel-collapse collapse ">
                                                                 <div class="panel-body">
                                                                     Answer 5
+                                                                    Uninterrupted Internet connectivity will be required during the submission of the source code
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -228,7 +234,7 @@ EventAsset::register($this);
 
 
     <?php $this->endBody() ?>
-    </body>
+</body>
 
-    </html>
+</html>
 <?php $this->endPage() ?>
