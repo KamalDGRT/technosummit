4c5a7517524929dbc756b9c905a949e9fcd18b08
Kamal Sharma <kamaldgrt@gmail.com>
2020-08-06 20:39:27 +0530

    upload each event empty file

--- backend/views/eventlist/index.php
+++ backend/views/eventlist/index.php
@@ -617,6 +617,29 @@ $this->title = 'Eventlist Home';
                         </a>
                     </div>
 
+                    <div class="col-md-6 col-xl-3 mb-4">
+                        <a href="<?php echo Url::to(['/eventlist/completelist']) ?>">
+                            <div class="card shadow border-left-primary py-2">
+
+                                <div class="card-body">
+                                    <div class="row align-items-center no-gutters">
+                                        <div class="col mr-2">
+                                            <div class="text-uppercase text-primary font-weight-bold text-xs mb-1">
+                                            </div>
+                                            <div class="text-dark font-weight-bold h5 mb-0">
+                                                <span>Complete List</span>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="far fa-sticky-note fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </a>
+                    </div>
+
                 </div>
 
             </div>
