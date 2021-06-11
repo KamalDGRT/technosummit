11b243113d75c3f6a36a684ed7111df49f2799a6
KamalDGRT <kamaldgrt@gmail.com>
2021-06-11 09:08:14 +0530

    fix timer for now

--- js/timer.js
+++ js/timer.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
     var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = 'August 08 2020 00:00:00 GMT+0530';
+var deadline = 'August 08 2021 00:00:00 GMT+0530';
 initializeClock('clockdiv', deadline);
